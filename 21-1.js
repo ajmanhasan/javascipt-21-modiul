9fa7c6affeb366797e841055c5f59129be5677b4
ajman hasan <ajmanhasan01@gmail.com>
2025-02-13 00:33:41 +0600

    first commit

---
+++ 21-1.js
@@ -0,0 +1,17 @@
+const pri = 45;
+
+function fanOff(){
+    console.log('bosa theka uthe dara');
+    console.log('walks twoamoro the switch');
+    console.log('click the switch to off of the fan');
+}
+
+
+fanOff();
+
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min gost and gost');
+}
+brushTeeth();
---
+++ 21-3-paramiter.js
@@ -0,0 +1,26 @@
+function vaaTkhao(){
+    console.log('hand arite');
+    console.log('boso');
+    console.log('tor nanier hada');
+
+}
+vaaTkhao();
+
+
+function square(number){
+console.log(number);
+console.log('valo of the number', number);
+const brogo = number * number;
+console.log('fdgg ggfg dfggf gegge', brogo)
+
+}
+
+square(4);
+square(12);
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+add(12, 34);
